39c5b48c0a45a2b4c84d2897f13a539d5e5db66a
none <none@gmail.com>
2024-08-21 18:57:22 -0500

    Solución

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -7,28 +7,36 @@ import java.util.Scanner;
  * @author danielsanchez
  */
 public class Calculadora {
+
     public static double calcular(int numero1, int numero2, char operacion) {
         double respuesta = 0;
         switch (operacion) {
-            case '+' :
+            case '+':
                 // Operación suma:
                 respuesta = numero1 + numero2;
                 break;
-            case '-' :
+            case '-':
                 // Operación resta:
                 respuesta = numero1 - numero2;
                 break;
-            case '*' :
+            case '*':
                 // Operación multiplicación:
                 respuesta = numero1 * numero2;
                 break;
-            case '/' :
+            case '/':
                 // Operación división:
                 respuesta = (double) numero1 / numero2;
                 break;
-            
+            case '^':
+                // Operación potencia:
+                respuesta = Math.pow(numero1, numero2);
+                break;
+            case '%':
+                // Operación modulo:
+                respuesta = (double) numero1 % numero2;
+                break;
+
             // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
-                
             default:
                 throw new IllegalArgumentException("Operación inválida.");
         }
@@ -37,18 +45,18 @@ public class Calculadora {
 
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-        
+
         // Lectura del valor de 2 variables enteras por consola:
         System.out.println("Ingrese el número 1");
         int numero1 = lector.nextInt();
         System.out.println("Ingrese el número 2");
         int numero2 = lector.nextInt();
-        
+
         // Lectura de la operación a realizar
         // TODO: DEBES ADICIONAR EL SÍMBOLO DE ^ Y % PARA CALCULAR LA POTENCIA Y EL MÓDULO
         System.out.println("Ingrese la operación (+, -, *, /)");
         char operacion = lector.next().charAt(0);
-        
+
         double respuesta = calcular(numero1, numero2, operacion);
         System.out.println("La respuesta es " + respuesta);
     }
